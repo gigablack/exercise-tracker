f6a026a03c59cb24b08bc68ae33ebf59a74d96ca
gigablack <tenchu96@gmail.com>
2019-02-10 23:23:45 -0430

    root route added

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const newUserController = require('../Controllers/newUserController')
 const newExerciseController = require('../Controllers/newExerciseController')
 const exerciseLogController = require('../Controllers/exerciseLogController')
+const path = require('path')
 
 router.get('/',(req,res)=>{
-    res.send('Hello Express')
+    res.sendFile(path.join(__dirname,'../index.html'))
 })
 
 router.post('/api/excercise/new-user',newUserController)
